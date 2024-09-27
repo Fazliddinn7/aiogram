af3588a4aaba277f5e23b0362a1de1bfeb5b9aef
Fazliddinn7 <fazliddingadoyev034@gmail.com>
2024-09-27 10:55:44 +0500

    shutdown and start

---
+++ lesson_1/main.py
@@ -0,0 +1,38 @@
+import logging
+import os
+import sys
+
+from aiogram import Bot, Dispatcher, types
+from asyncio import run
+
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+BOT_TOKEN = os.getenv('TOKEN')
+
+dp = Dispatcher()
+
+
+@dp.startup()
+async def start_answer(bot: Bot):
+    await bot.send_message(6318392141, "Bot ishga tushdi ✅")
+
+
+@dp.shutdown()
+async def shutdown_answer(bot: Bot):
+    await bot.send_message(6318392141, 'Bot ishdan to\'xtadi ❌')
+
+
+@dp.message()
+async def echo(msg: types.Message, bot: Bot):
+    await msg.copy_to(chat_id=msg.chat.id)
+
+
+async def start():
+    bot = Bot('7061245022:AAFgL4MH4BDhwkwGUri1JlD_cSsuLkUjp8g', default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot, polling_timeout=1)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    run(start())
